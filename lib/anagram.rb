5085361a01270fd22ba3cd033975647650e2caa0
Trayshawn <traytwebb14@yahoo.com>
2020-11-17 18:55:04 -0600

    commit tree 7de3061102dd37dd60b766dbda47512b37def5b3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+    def match(string)
+        string.select do |ana|
+            ana.split("").sort == word.split("").sort   
+        end
+      
+    end
+end
